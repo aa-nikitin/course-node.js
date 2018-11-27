7b4576a0690564af2ad97db8c09e0506e51c1f44
aa-nikitin <a.nikitin91@mail.ru>
2018-11-27 17:44:22 +0300

    start hw2

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-const process = require('process');
-console.log(process.env);
+const fs = require('fs');
+const readDir = require('./modules/readDir');
+const fromDir = process.argv[2];
+const toDir = process.argv[3] ? process.argv[3] : './result';
+
+fs.mkdir(toDir, err => {
+  if (err) {
+    console.log('Ошибка создания итоговой папки');
+    return null;
+  } else {
+    readDir(fromDir, () => {});
+  }
+});
+
+// const stats = async file => {
+//   return fs.mkdir(file, err => {
+//     if (err) {
+//       console.log('error');
+//       return err;
+//     } else {
+//       console.log('succsess');
+//     }
+//   });
+// };
+
+// const f = async () => {
+//   try {
+//     let response = await stats(toDir);
+//     return response;
+//   } catch (err) {
+//     return err;
+//   }
+// };
+
+// f();
